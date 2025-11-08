95e349afcb8b1ed9f74d05683a38b6f10df789df
shantaramtomsk <indorsoftwork@gmail.com>
2025-11-08 17:00:25 +0700

    1

---
+++ Matplotlib.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Простая визуализация данных
+
+x = [1, 2, 3, 4, 5]
+
+y = [1, 4, 9, 16, 25]
+
+plt.plot(x, y)
+
+plt.xlabel('X')
+
+plt.ylabel('Y')
+
+plt.title('График X против Y')
+
+plt.show()
---
+++ NumPy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Создание массива
+
+array = np.array([1, 2, 3, 4, 5])
+
+# Выполнение математических операций
+
+array_sum = np.sum(array)  # Сумма элементов
+
+array_mean = np.mean(array)  # Среднее значение
+print(array_sum)
+print(array_mean)
